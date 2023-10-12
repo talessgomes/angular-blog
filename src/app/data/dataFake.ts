bfffdf2c89cfcf80c9b03f456902d4b934d10b93
talessgomes <tales.santos.gomes@outlook.com>
2023-10-12 19:35:43 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,37 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo": "Homem-Aranha",
+    "title": "Homem-Aranha",
+    "description": "Sony anuncia novo filme do Homem-Aranha que vai ser interpretado pelo Miles Morales",
+    "photoCover": "https://img.olhardigital.com.br/wp-content/uploads/2023/06/homem-aranha-atraves-do-aranhaverso.jpg"
+  },
+  {
+    "id":"2",
+    "photo": "Heróis na foto",
+    "title": "Nova série anunciada no Disney plus",
+    "description": "Disney plus acaba de anunciar que vai ser lançado novas séries no seu streaming",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "photo": "Heróis na foto",
+    "title": "Parece que vai ter mais coisas da Marvel",
+    "description": "Parece que além das outras séries que vão lançar no Disney plus, vamos poder contar com mais uma remessa extra",
+    "photoCover": "https://legadoplus.com.br/wp-content/uploads/2023/08/revelado-a-serie-da-marvel-mais-assistida-no-disney-legadoplus.webp"
+  },
+  {
+    "id":"4",
+    "photo": "Série WandaVision",
+    "title": "Wanda vai ter que enfrentar novas dificuldades na sua nova série",
+    "description": "Em breve vamos poder contar com uma nova série. Seu nome será WandaVision",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6DAA31D2EAD43853A24CCBD79A8C38E0EF299DEBEF59CC4100544C65AACC252D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"5",
+    "photo": "Teaser Filme Thor",
+    "title": "Bomba! Lançou novo Teaser do filme do Thor",
+    "description": "Teaser de thor aprontando a maior loucura com os seus amigos",
+    "photoCover": "https://i.ytimg.com/vi/waTob1IM4UM/maxresdefault.jpg"
+  }
+]
